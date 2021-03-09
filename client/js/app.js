cb815062c5e12ed3bfac834e07dab08954ca4b02
Tehila <tehilaschechter@gmail.com>
2021-03-09 23:45:20 +0200

    block out table for all columns

--- client/js/app.js
+++ client/js/app.js
@@ -1,8 +1,35 @@
+function ParkingEntry(){
+    this.license = license;
+    this.timeIn = timeIn;
+    this.timeOut = timeOut;
+    this.duration = calculateDuration(timeIn, timeOut);
+    this.price = calculatePrice(this.duration);
+}
+
+function calculateDuration(timeIn, timeOut){
+    return timeOut - timeIn;
+}
+
+function calculatePrice(duration){
+    const ratePerHour = 2.99;
+    const chargeableTime = duration - 1;
+
+    return chargeableTime * ratePerHour;
+}
+
+function initializeParkingEntries(parkingEntryData){
+    var parkingEntries = [];
+    for (let entry of parkingEntryData){
+        parkingEntries.push(new ParkingEntry(JSON.parse(entry)))
+    }
+}
+
 function generateTableHead(table, data) {
     let thead = table.createTHead();
     let row = thead.insertRow();
     for (let key of data) {
         let th = document.createElement("th");
+        th.style.border = '1px solid black';
         let text = document.createTextNode(key);
         th.appendChild(text);
         row.appendChild(th);
@@ -10,14 +37,50 @@ function generateTableHead(table, data) {
 }
 
 function generateTable(table, data) {
-    for (let element of data) {
+    for (let entry of data) {
         let row = table.insertRow();
-        for (key in element) {
+        /*for (key in entry) {
             let cell = row.insertCell();
-            let text = document.createTextNode(element[key]);
+            cell.style.border = '1px solid black';
+            let text = document.createTextNode(entry[key]);
             cell.appendChild(text);
-        }
+        }*/
+        let licenseCell = row.insertCell();
+        licenseCell.style.border = '1px solid black';
+        let licenseText = document.createTextNode(entry.license);
+        licenseCell.appendChild(licenseText);
+
+        let priceCell = row.insertCell();
+        priceCell.style.border = '1px solid black';
+        let priceText = document.createTextNode(entry.price);
+        priceCell.appendChild(priceText);
+
+        let durationCell = row.insertCell();
+        durationCell.style.border = '1px solid black';
+        let durationText = document.createTextNode(entry.duration);
+        durationCell.appendChild(durationText);
+
+        let timeInCell = row.insertCell();
+        timeInCell.style.border = '1px solid black';
+        let timeInText = document.createTextNode(entry.timeIn);
+        timeInCell.appendChild(timeInText);
+
+        let timeOutCell = row.insertCell();
+        timeOutCell.style.border = '1px solid black';
+        let timeOutText = document.createTextNode(entry.timeOut);
+        timeOutCell.appendChild(timeOutText);
     }
+}
+
+function createTable(data){
+    let table = document.querySelector("table");
+    let headers = Object.keys(data[0]);
+
+    table.style.width  = '100px';
+    table.style.border = '1px solid black';
+
+    generateTable(table, data);
+    //generateTableHead(table, headers); 
 }
 
 function httpGetAsync(url, callback)
@@ -32,13 +95,4 @@ function httpGetAsync(url, callback)
     xmlHttp.send(null);
 }
 
-function createTable(data){
-    let table = document.querySelector("table");
-    let headers = Object.keys(data[0]);
-    generateTable(table, data);
-    generateTableHead(table, headers); 
-}
-
 httpGetAsync("http://localhost:4200/api/event", createTable)
-
-
